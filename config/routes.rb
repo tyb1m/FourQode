bf319ff1cea8322bd808a2e4587e0f6190e342ae
Kazuki <edak1209@gmail.com>
2019-04-17 21:52:42 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # 仮置き
+  root 'users#index'
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
