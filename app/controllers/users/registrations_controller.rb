552b82311a98545998cac9e2da0d93d5016ba0e6
nozomi71 <skn.miu@gmail.com>
2019-05-05 22:54:42 +0900

    登録完了画面マークアップ完了

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -39,9 +39,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-  def after_sign_up_path_for(resource)
-    create_path
- end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
