ce1cd89a6e6da69b99474787581e4427d0e9c7b9
RyuSeungChanne <ryuseungchanne@ryuushousannoMacBook-puro.local>
2020-01-29 20:14:27 +0900

    7 guide view

--- app/assets/javascripts/guideSidebar.js
+++ app/assets/javascripts/guideSidebar.js
@@ -1,11 +1,53 @@
-// $(function() {
-//     $('.startSideBarsListLink').click(function(){
-//         $('.startSideBarsSub').toggleClass();
-//     });
-// });
+$(function() {
+    $('.firstGuide').click(function(){
+        $('.SideBarSub_firstGuide').slideDown();
+        $('#fa-firstGuide').css({
+          "transform" : "rotate(90deg)"
+        });
+    });
+});
 
 $(function() {
-    $('.startNavList').on('click', function() {
-        $('.startSideBarsListLink').toggleClass('is-active');
+    $('.questionGuide').click(function(){
+        $('.SideBarSub_questions').slideDown();
+        $('#fa-questions').css({
+          "transform" : "rotate(90deg)"
+        });
+    });
+});
+
+$(function() {
+    $('.solutionGuide').click(function(){
+        $('.SideBarSub_solution').slideDown();
+        $('#fa-solution').css({
+          "transform" : "rotate(90deg)"
+        });
+    });
+});
+
+$(function() {
+    $('.coinGuide').click(function(){
+        $('.SideBarSub_coin').slideDown();
+        $('#fa-coin').css({
+          "transform" : "rotate(90deg)"
+        });
+    });
+});
+
+$(function() {
+    $('.registerGuide').click(function(){
+        $('.SideBarSub_register').slideDown();
+        $('#fa-register').css({
+          "transform" : "rotate(90deg)"
+        });
+    });
+});
+
+$(function() {
+    $('.cancelGuide').click(function(){
+        $('.SideBarSub_cancel').slideDown();
+        $('#fa-cancel').css({
+          "transform" : "rotate(90deg)"
+        });
     });
 });
